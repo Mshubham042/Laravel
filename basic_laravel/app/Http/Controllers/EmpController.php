d761a4a0afb529621d9b10328ce1332f67f6204a
Mshubham042 <shubhammishra.dcs22n@vnsgu.ac.in>
2021-12-07 15:49:14 +0530

    laravelbasics

---
+++ basic_laravel/app/Http/Controllers/EmpController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EmpController extends Controller
+{
+   public function showdata(Request $request)
+   {
+    //    return $request->get('username');
+    // for returning a single request
+
+   }
+}
